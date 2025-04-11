820d60fd8b3a886ec192d1be2e5517701b6f9fe3
Alexander Familia <dalex2525@hotmail.com>
2025-04-11 18:20:08 -0400

    feat: add PositionSeedData migration and seed initial data for Positions

---
+++ JobPositions-backend/JobPositions.Data/Migrations/20250411221826_PositionSeedData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace JobPositions.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PositionSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Positions",
+                columns: new[] { "Id", "Budget", "CreatedDate", "DepartmentId", "ModifiedDate", "Number", "RecruiterId", "StatusId", "Title" },
+                values: new object[,]
+                {
+                    { 1, 6000m, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, null, 200, 1, 2, "Full-Stack Developer" },
+                    { 2, 3000m, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, null, 201, 2, 1, "QA Engineer" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Positions",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Positions",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
