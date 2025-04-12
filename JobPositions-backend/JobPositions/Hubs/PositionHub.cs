e2513b7c916930c89afd37674a8ab58c184b148d
Alexander Familia <dalex2525@hotmail.com>
2025-04-12 00:37:43 -0400

    refactor: remove unused IPositionService dependency and clean up PositionHub

--- JobPositions-backend/JobPositions/Hubs/PositionHub.cs
+++ JobPositions-backend/JobPositions/Hubs/PositionHub.cs
@@ -1,16 +1,9 @@
-﻿using JobPositions.Business.Abstract;
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.SignalR;
 
 namespace JobPositions.Hubs
 {
     public class PositionHub : Hub
     {
-        //private readonly IPositionService _positionService;
-        //public PositionHub(IPositionService positionService)
-        //{
-        //    _positionService = positionService;
-        //}
-
         public async Task DataChange()
         {
             await Clients.All.SendAsync("DataChanged");
