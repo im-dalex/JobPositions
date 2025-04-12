eeec5c826e247e8179942b5a7bc997a627e64066
Alexander Familia <dalex2525@hotmail.com>
2025-04-12 00:20:30 -0400

    feat: add Toaster component for enhanced notification display in App

--- job-positions-frontend/src/App.tsx
+++ job-positions-frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import "./App.css";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
     <div className="p-8">
       <h1 className="text-4xl font-bold mb-7">Job Positions</h1>
+      <Toaster expand visibleToasts={3} position="top-right" richColors />
       <RouterProvider router={router} />
     </div>
   );
